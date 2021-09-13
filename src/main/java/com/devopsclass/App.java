db11c0c0e1116ac471afaccdb8d7d124e46c9375
vivekjcloud <79889915+vivekjcloud@users.noreply.github.com>
2021-09-13 09:21:42 +0530

    auto pulling of code

--- src/main/java/com/devopsclass/App.java
+++ src/main/java/com/devopsclass/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!, first change" );
+        System.out.println( "Hello World!, this time code has been pulled automatically" );
     }
 }
